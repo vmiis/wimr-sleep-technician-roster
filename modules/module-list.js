2b58ad6f593974b7f54de576afcd0da4168366b9
Zhiming Zhou <zzm.vmiis@gmail.com>
2022-02-10 14:29:25 +1100

    update

---
+++ modules/module-list.js
@@ -0,0 +1,14 @@
+(function(){
+    //-------------------------------------------------------------------------------------
+    var p=""; //put a unique prefix to avoid conflict with others 
+    var H=$vm.hosting_path+"/modules";
+    var m=$vm.module_list;
+    //var api="wimr-staff-roster";
+    //-------------------------------------------------------------------------------------
+    m[p+"search-staff"]     ={url:H+"/business/staff/page.html", prefix:p,router:1};
+    m[p+"roster-data"]      ={url:H+"/business/roster/data.html",       Table:"staff-roster",  form_module:"roster-form"},
+    m[p+"roster-form"]      ={url:H+"/business/roster/form.html",       Table:"staff-roster"}
+    m[p+"month"]            ={url:H+"/business/calendar/month.html",    Table:"staff-roster","booking":p+"roster-form",router:1};
+    m[p+"week"]             ={url:H+"/business/calendar/week.html",     Table:"staff-roster","booking":p+"roster-form","booking2":p+"roster-form-2",router:1};
+    m[p+"day"]              ={url:H+"/business/calendar/day.html",      Table:"staff-roster","booking":p+"roster-form",router:1};
+})();
